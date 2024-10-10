8f0deac1f7f187477b3af216294e9f44422b52e3
JEONG-JIN-GYO <202021035@sangmyung.kr>
2024-10-10 16:35:54 +0900

    Delete tripMateApp directory

--- tripMateApp/src/Components/ModalPortal.jsx
+++
@@ -1,6 +0,0 @@
-import { createPortal } from "react-dom";
-
-export default function ModalPortal({ children }) {
-  const element = document.getElementById("modal");
-  return createPortal(children, element);
-}
--- tripMateApp/src/Pages/mytrip.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Description_mytrip from "../Mytrip";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Description_mytrip />
-  </React.StrictMode>
-);
