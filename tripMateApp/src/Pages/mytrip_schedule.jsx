bd152574dd8ca1a3de0b3e6ccc4d67d17e00867c
JEONG-JIN-GYO <202021035@sangmyung.kr>
2024-10-10 15:47:19 +0900

    Add files via upload

---
+++ tripMateApp/src/Pages/mytrip_schedule.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Description_mytrip from "../trip_list.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Description_mytrip />
+  </React.StrictMode>
+);
